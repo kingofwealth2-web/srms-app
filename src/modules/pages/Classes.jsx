bc1463d639e83a3d760630ab8816bf2c517b7066
Prince <kingofwealth2@gmail.com>
2026-03-05 08:19:20 +0000

    fix classes crash

--- src/modules/pages/Classes.jsx
+++ src/modules/pages/Classes.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { supabase } from '../../supabase'
 import { useIsMobile } from '../lib/hooks'
 import { ROLE_META } from '../lib/constants'
